2cf8927939f5bb4f6631659802142ac50f59f0d9
Thiago Beltrami Dias Batista <tbdbatista@gmail.com>
2023-04-25 13:24:54 -0300

    implement developer class methods

--- src/br/com/dio/desafio/dominio/Developer.java
+++ src/br/com/dio/desafio/dominio/Developer.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Developer {
@@ -9,11 +10,27 @@ public class Developer {
     private Set<Content> subscribedContent = new LinkedHashSet<>();
     private Set<Content> finishedContent = new LinkedHashSet<>();
 
-    public void subscribeToBootcamp(Bootcamp bootcamp) {}
+    public void subscribeToBootcamp(Bootcamp bootcamp) {
+        this.subscribedContent.addAll(bootcamp.getBootcampContents());
+        bootcamp.getSubscribedDevelopers().add(this);
+    }
 
-    public void progress() {}
+    public void progress() {
+        Optional<Content> content = this.subscribedContent.stream().findFirst();
+        if (content.isPresent()) {
+            this.finishedContent.add(content.get());
+            this.subscribedContent.remove(content.get());
+        } else {
+            System.err.println("Dev não inscrito em nenhum conteúdo.");
+        }
+    }
 
-    public void calculateTotalExp() {}
+    public double calculateTotalExp() {
+        return this.finishedContent
+                .stream()
+                .mapToDouble(Content::calculateExp)
+                .sum();
+    }
 
     public String getName() {
         return name;
@@ -44,7 +61,9 @@ public class Developer {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Developer developer = (Developer) o;
-        return Objects.equals(name, developer.name) && Objects.equals(subscribedContent, developer.subscribedContent) && Objects.equals(finishedContent, developer.finishedContent);
+        return Objects.equals(name, developer.name)
+                && Objects.equals(subscribedContent, developer.subscribedContent)
+                && Objects.equals(finishedContent, developer.finishedContent);
     }
 
     @Override
